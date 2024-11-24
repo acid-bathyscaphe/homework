c5e3dcff7e22dd078c25a9d67a1265c3bbd5434b
acid-bathyscaphe <Sisterovandrej314@gmail.com>
2024-11-24 18:21:44 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+result1 = 2*2+2
+result2 = 2*(2+2)
+print(result1)
+print(result2)
+print(result1 == result2)
+#4th program
+print(int(float('123.456') * 10) % 10)
